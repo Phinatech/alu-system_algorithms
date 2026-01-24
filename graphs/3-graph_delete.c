fa178d69395ee4b32508d54e9a153c8f2d9c53ec
Phinatech <c.ugbo@alustudent.com>
2026-01-24 12:50:05 +0200

    remove executable

--- graphs/3-graph_delete.c
+++ graphs/3-graph_delete.c
@@ -1,14 +1,39 @@
 #include "graphs.h"
+#include <stdlib.h>
 
 /**
- * graph_delete - completely deallocates a graph_t
- * @graph: pointer to the graph_t to be deleted
+ * graph_delete - Deallocates a graph
+ * @graph: Pointer to the graph to delete
  */
 void graph_delete(graph_t *graph)
 {
+	size_t i, j;
+	vertex_t *v;
+	edge_t *e, *temp;
+
 	if (!graph)
 		return;
-	FREE(vertex_t, v, graph->vertices, free(v->content);
-	     FREE(edge_t, e, v->edges, NULL;););
+
+	for (i = 0; i < graph->nb_vertices; i++)
+	{
+		v = graph->vertices[i];
+
+		for (j = 0; j < v->nb_edges; j++)
+		{
+			e = v->edges[j];
+			while (e)
+			{
+				temp = e->next;
+				free(e);
+				e = temp;
+			}
+		}
+
+		free(v->edges);
+		free(v->content);
+		free(v);
+	}
+
+	free(graph->vertices);
 	free(graph);
 }

75a0c72ea604e1baf3794b0765846c662f1bbd21
Phinatech <c.ugbo@alustudent.com>
2026-01-28 02:15:20 +0200

    implement heap data structure with creation and integer comparison functions

---
+++ huffman_coding/heap/heap_create.c
@@ -0,0 +1,23 @@
+#include "heap.h"
+#include <stdlib.h>
+
+/**
+ * heap_create - Creates a Heap data structure
+ * @data_cmp: Pointer to a comparison function
+ *
+ * Return: Pointer to the created heap_t structure, or NULL if it fails
+ */
+heap_t *heap_create(int (*data_cmp)(void *, void *))
+{
+	heap_t *heap;
+
+	heap = malloc(sizeof(heap_t));
+	if (heap == NULL)
+		return (NULL);
+
+	heap->size = 0;
+	heap->root = NULL;
+	heap->data_cmp = data_cmp;
+
+	return (heap);
+}

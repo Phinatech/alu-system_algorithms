4aa3d3d096db9af15d10840d46b2d300925c9e24
Phinatech <c.ugbo@alustudent.com>
2026-01-28 03:43:11 +0200

    pushing

--- huffman_coding/heap/heap_insert.c
+++ huffman_coding/heap/heap_insert.c
@@ -11,96 +11,94 @@
 binary_tree_node_t *binary_tree_node(binary_tree_node_t *parent, void *data);
 
 /**
- * get_parent - Gets the parent node for the next insertion
- * @root: Root of the tree
- * @index: Index where the new node will be inserted
- *
- * Return: Pointer to the parent node
+ * swap_data - Swaps data between two nodes
+ * @a: First node
+ * @b: Second node
  */
-binary_tree_node_t *get_parent(binary_tree_node_t *root, size_t index)
+void swap_data(binary_tree_node_t *a, binary_tree_node_t *b)
 {
-	size_t parent_idx;
-	char path[64];
-	int i, depth;
+	void *tmp = a->data;
+
+	a->data = b->data;
+	b->data = tmp;
+}
+
+/**
+ * heapify_up - Restores min heap property
+ * @heap: Pointer to heap
+ * @node: Node to heapify
+ */
+void heapify_up(heap_t *heap, binary_tree_node_t *node)
+{
+	while (node->parent && heap->data_cmp(node->data, node->parent->data) < 0)
+	{
+		swap_data(node, node->parent);
+		node = node->parent;
+	}
+}
+
+/**
+ * get_node - Get node at index in level order
+ * @root: Root of tree
+ * @index: Index to find
+ *
+ * Return: Node at index
+ */
+binary_tree_node_t *get_node(binary_tree_node_t *root, size_t index)
+{
+	size_t mask = 1;
 
 	if (index == 0)
-		return (NULL);
-
-	parent_idx = (index - 1) / 2;
-
-	if (parent_idx == 0)
 		return (root);
 
-	/* Build path in binary */
-	depth = 0;
-	for (i = parent_idx; i > 0; i >>= 1)
-		path[depth++] = (i & 1) ? 'R' : 'L';
+	while (mask <= index)
+		mask <<= 1;
+	mask >>= 2;
 
-	/* Traverse from root following the path (in reverse) */
-	for (i = depth - 2; i >= 0; i--)
+	while (mask)
 	{
-		if (path[i] == 'R')
-			root = root->right;
-		else
-			root = root->left;
+		root = (index & mask) ? root->right : root->left;
+		mask >>= 1;
 	}
 
 	return (root);
 }
 
 /**
- * heapify_up - Bubbles up a node to maintain min heap property
- * @heap: Pointer to the heap
- * @node: Node to bubble up
- */
-void heapify_up(heap_t *heap, binary_tree_node_t *node)
-{
-	void *temp;
-
-	while (node->parent && heap->data_cmp(node->data, node->parent->data) < 0)
-	{
-		temp = node->data;
-		node->data = node->parent->data;
-		node->parent->data = temp;
-		node = node->parent;
-	}
-}
-
-/**
- * heap_insert - Inserts a value in a Min Binary Heap
- * @heap: Pointer to the heap
- * @data: Pointer to data to store in the new node
+ * heap_insert - Inserts value in Min Binary Heap
+ * @heap: Pointer to heap
+ * @data: Data to insert
  *
- * Return: Pointer to the created node, or NULL on failure
+ * Return: Pointer to created node, or NULL on failure
  */
 binary_tree_node_t *heap_insert(heap_t *heap, void *data)
 {
-	binary_tree_node_t *new_node, *parent;
+	binary_tree_node_t *new, *parent;
 
 	if (!heap || !data)
 		return (NULL);
 
-	new_node = binary_tree_node(NULL, data);
-	if (!new_node)
+	new = binary_tree_node(NULL, data);
+	if (!new)
 		return (NULL);
 
 	if (heap->size == 0)
 	{
-		heap->root = new_node;
+		heap->root = new;
 		heap->size = 1;
-		return (new_node);
+		return (new);
 	}
 
-	parent = get_parent(heap->root, heap->size);
-	new_node->parent = parent;
+	parent = get_node(heap->root, (heap->size - 1) / 2);
+	new->parent = parent;
 
-	if ((heap->size & 1) == 1)
-		parent->left = new_node;
+	if (heap->size % 2 == 1)
+		parent->left = new;
 	else
-		parent->right = new_node;
+		parent->right = new;
 
 	heap->size++;
-	heapify_up(heap, new_node);
+	heapify_up(heap, new);
 
-	return (new_node);
+	return (new);
 }

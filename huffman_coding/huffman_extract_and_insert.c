dfd5b8a0e63ac44876fb3ea75c156600fc29feb1
Phinatech <c.ugbo@alustudent.com>
2026-01-31 17:22:51 +0200

    pushing

--- huffman_coding/huffman_extract_and_insert.c
+++ huffman_coding/huffman_extract_and_insert.c
@@ -3,6 +3,41 @@
 #include <stdlib.h>
 
 /**
+ * create_parent_node - Creates a parent node from two child nodes
+ * @node1: First child node
+ * @node2: Second child node
+ *
+ * Return: Pointer to parent node, or NULL on failure
+ */
+binary_tree_node_t *create_parent_node(binary_tree_node_t *node1,
+		binary_tree_node_t *node2)
+{
+	binary_tree_node_t *parent;
+	symbol_t *sym1, *sym2, *parent_sym;
+
+	sym1 = (symbol_t *)(node1->data);
+	sym2 = (symbol_t *)(node2->data);
+
+	parent_sym = symbol_create(-1, sym1->freq + sym2->freq);
+	if (!parent_sym)
+		return (NULL);
+
+	parent = binary_tree_node(NULL, parent_sym);
+	if (!parent)
+	{
+		free(parent_sym);
+		return (NULL);
+	}
+
+	parent->left = node1;
+	parent->right = node2;
+	node1->parent = parent;
+	node2->parent = parent;
+
+	return (parent);
+}
+
+/**
  * huffman_extract_and_insert - Extracts two nodes and inserts a new one
  * @priority_queue: Pointer to the priority queue
  *
@@ -11,7 +46,6 @@
 int huffman_extract_and_insert(heap_t *priority_queue)
 {
 	binary_tree_node_t *node1, *node2, *parent;
-	symbol_t *sym1, *sym2, *parent_sym;
 
 	if (!priority_queue || priority_queue->size < 2)
 		return (0);
@@ -24,31 +58,16 @@ int huffman_extract_and_insert(heap_t *priority_queue)
 	if (!node2)
 		return (0);
 
-	sym1 = (symbol_t *)(node1->data);
-	sym2 = (symbol_t *)(node2->data);
-
-	parent_sym = symbol_create(-1, sym1->freq + sym2->freq);
-	if (!parent_sym)
-		return (0);
-
-	parent = binary_tree_node(NULL, parent_sym);
+	parent = create_parent_node(node1, node2);
 	if (!parent)
-	{
-		free(parent_sym);
 		return (0);
-	}
-
-	parent->left = node1;
-	parent->right = node2;
-	node1->parent = parent;
-	node2->parent = parent;
 
 	if (!heap_insert(priority_queue, parent))
 	{
 		parent->left = NULL;
 		parent->right = NULL;
+		free(parent->data);
 		free(parent);
-		free(parent_sym);
 		return (0);
 	}
 
